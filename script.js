c3d8f0faa0d22635624a18ab0c45b039821d3b9a
Abdul Quadir <60433139+Abdul5@users.noreply.github.com>
2023-07-18 03:06:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function fibonacci(num) {
-  if (num <= 1) {
-    return num;
-  } else {
-    return fibonacci(num - 1) + fibonacci(num - 2);
-  }
+// your code here
+	if(num === 1) return 0;
+	if(num === 2) return 1;
+
+	return fibonacci(num-2)+fibonacci(num-1);
 }
+// let a = prompt("Enter a number");
+// let ans = fibonacci(a);
+// alert("Your fib no. is "+ans);
 
 module.exports = fibonacci;
